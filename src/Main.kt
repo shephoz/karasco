2fa8b3c5f690b89583517e87ae59d256e8062232
wozniak385 <clavier.nof@wozniak.jp>
2019-08-23 12:47:29 +0900

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println(calculate(12345))
+}
+
+fun calculate(score: Int): List<Hand> {
+    val hands = mutableListOf<Hand>()
+    return hands
+}
+
+enum class Hand(point: Int) {
+    TwoPairs(1), ThreeCards(1), Straight(2), FullHouse(2), FourCards(3),
+    HighScore(1), Push(2), BlackJack(2), High(1), Low(1),
+    Flash(1), ThreeSevens(2)
+}
+
+enum class SpecialHand(bottles: Int) {
+    StraightFlash(1), FiveCards(1),
+    AllSeven(2), Perfect(2)
+}
