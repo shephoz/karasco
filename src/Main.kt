79387ec396ae3f3bf055653815bfedb2d8385cfb
wozniak385 <clavier.nof@wozniak.jp>
2019-08-25 15:28:53 +0900

    added interface Hand that contains NormalHand, SpecialHand

--- src/Main.kt
+++ src/Main.kt
@@ -9,13 +9,13 @@ fun calculate(score: Int): Set<Hand> {
     val sum: Int = digits.sum()
     when (sum) {
         in 0..17 -> {
-            hands.add(Hand.Low)
+            hands.add(NormalHand.Low)
         }
         21 -> {
-            hands.add(Hand.BlackJack)
+            hands.add(NormalHand.BlackJack)
         }
         in 37..45 -> {
-            hands.add(Hand.High)
+            hands.add(NormalHand.High)
         }
     }
 
@@ -29,13 +29,15 @@ fun calculate(score: Int): Set<Hand> {
     return hands
 }
 
-enum class Hand(point: Int) {
+interface Hand
+
+enum class NormalHand(point: Int) : Hand {
     TwoPairs(1), ThreeCards(1), Straight(2), FullHouse(2), FourCards(3),
     HighScore(1), Push(2), BlackJack(2), High(1), Low(1),
     Flash(1), ThreeSevens(2)
 }
 
-enum class SpecialHand(bottles: Int) {
+enum class SpecialHand(bottles: Int) : Hand {
     StraightFlash(1), FiveCards(1),
     AllSeven(2), Perfect(2)
 }
