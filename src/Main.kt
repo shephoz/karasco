77515b46c30411d45007dcb64b82595ada285080
wozniak385 <clavier.nof@wozniak.jp>
2019-08-25 16:54:11 +0900

    separated file of Hand interface

--- src/Main.kt
+++ src/Main.kt
@@ -101,16 +101,3 @@ class Occurrence(val digit: Int, var times: Int) {
     fun increment() = this.times++
     override fun toString(): String = "$digit($times)"
 }
-
-interface Hand
-
-enum class NormalHand(point: Int) : Hand {
-    TwoPairs(1), ThreeCards(1), Straight(2), FullHouse(2), FourCards(3),
-    HighScore(1), Push(2), BlackJack(2), High(1), Low(1),
-    Flash(1), ThreeSevens(2)
-}
-
-enum class SpecialHand(bottles: Int) : Hand {
-    StraightFlash(1), FiveCards(1),
-    AllSeven(2), Perfect(2)
-}
