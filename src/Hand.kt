67883e1afefb780eb504ea4e41df6ca9b3899577
wozniak385 <clavier.nof@wozniak.jp>
2019-08-25 20:08:34 +0900

    added `val` to enum property to read them from outside

--- src/Hand.kt
+++ src/Hand.kt
@@ -2,13 +2,13 @@ package jp.wozniak.karasco
 
 interface Hand
 
-enum class NormalHand(point: Int) : Hand {
+enum class NormalHand(val point: Int) : Hand {
     TwoPairs(1), ThreeCards(1), Straight(2), FullHouse(2), FourCards(3),
     HighScore(1), Push(2), BlackJack(2), High(1), Low(1),
     Flush(1), ThreeSevens(2)
 }
 
-enum class SpecialHand(bottles: Int) : Hand {
+enum class SpecialHand(val bottles: Int) : Hand {
     StraightFlush(1), FiveCards(1),
     AllSeven(2), Perfect(2)
 }
