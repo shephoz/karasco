378cedfb7a58b4a640b2a1e77e944e59d225dac9
wozniak385 <clavier.nof@wozniak.jp>
2019-09-11 22:03:02 +0900

    introduced GET parameter of score

--- src/Application.kt
+++ src/Application.kt
@@ -2,19 +2,29 @@ package jp.wozniak.karasco
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.http.ContentType
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Location
+import io.ktor.locations.Locations
+import io.ktor.locations.get
 import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.routing
+
+@KtorExperimentalLocationsAPI
+@Location("/karasco")
+data class Score(val score: Double)
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@KtorExperimentalLocationsAPI
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(Locations)
     routing {
-        get("/") {
-            val hands = calculate(77.007)
+        get<Score> { score ->
+            val hands = calculate(score.score)
             val points = hands.filterIsInstance<NormalHand>().sumBy(NormalHand::point)
             val bottles = hands.filterIsInstance<SpecialHand>().sumBy(SpecialHand::bottles)
 
